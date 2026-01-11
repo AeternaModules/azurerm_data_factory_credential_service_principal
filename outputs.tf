6330d2df8084e6926e058bf15d85cd5316b15c46
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:41:50 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "data_factory_credential_service_principals" {
+  description = "All data_factory_credential_service_principal resources"
+  value       = azurerm_data_factory_credential_service_principal.data_factory_credential_service_principals
+}
+
