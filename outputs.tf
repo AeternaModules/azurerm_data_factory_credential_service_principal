c4538f9bb29539a5c8bb0599083029df86a2b6ba
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:43 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "data_factory_credential_service_principals" {
-  description = "All data_factory_credential_service_principal resources"
-  value       = azurerm_data_factory_credential_service_principal.data_factory_credential_service_principals
-}
 output "data_factory_credential_service_principals_annotations" {
-  description = "List of annotations values across all data_factory_credential_service_principals"
-  value       = [for k, v in azurerm_data_factory_credential_service_principal.data_factory_credential_service_principals : v.annotations]
+  description = "Map of annotations values across all data_factory_credential_service_principals, keyed the same as var.data_factory_credential_service_principals"
+  value       = { for k, v in azurerm_data_factory_credential_service_principal.data_factory_credential_service_principals : k => v.annotations }
 }
 output "data_factory_credential_service_principals_data_factory_id" {
-  description = "List of data_factory_id values across all data_factory_credential_service_principals"
-  value       = [for k, v in azurerm_data_factory_credential_service_principal.data_factory_credential_service_principals : v.data_factory_id]
+  description = "Map of data_factory_id values across all data_factory_credential_service_principals, keyed the same as var.data_factory_credential_service_principals"
+  value       = { for k, v in azurerm_data_factory_credential_service_principal.data_factory_credential_service_principals : k => v.data_factory_id }
 }
 output "data_factory_credential_service_principals_description" {
-  description = "List of description values across all data_factory_credential_service_principals"
-  value       = [for k, v in azurerm_data_factory_credential_service_principal.data_factory_credential_service_principals : v.description]
+  description = "Map of description values across all data_factory_credential_service_principals, keyed the same as var.data_factory_credential_service_principals"
+  value       = { for k, v in azurerm_data_factory_credential_service_principal.data_factory_credential_service_principals : k => v.description }
 }
 output "data_factory_credential_service_principals_name" {
-  description = "List of name values across all data_factory_credential_service_principals"
-  value       = [for k, v in azurerm_data_factory_credential_service_principal.data_factory_credential_service_principals : v.name]
+  description = "Map of name values across all data_factory_credential_service_principals, keyed the same as var.data_factory_credential_service_principals"
+  value       = { for k, v in azurerm_data_factory_credential_service_principal.data_factory_credential_service_principals : k => v.name }
 }
 output "data_factory_credential_service_principals_service_principal_id" {
-  description = "List of service_principal_id values across all data_factory_credential_service_principals"
-  value       = [for k, v in azurerm_data_factory_credential_service_principal.data_factory_credential_service_principals : v.service_principal_id]
+  description = "Map of service_principal_id values across all data_factory_credential_service_principals, keyed the same as var.data_factory_credential_service_principals"
+  value       = { for k, v in azurerm_data_factory_credential_service_principal.data_factory_credential_service_principals : k => v.service_principal_id }
 }
 output "data_factory_credential_service_principals_service_principal_key" {
-  description = "List of service_principal_key values across all data_factory_credential_service_principals"
-  value       = [for k, v in azurerm_data_factory_credential_service_principal.data_factory_credential_service_principals : v.service_principal_key]
+  description = "Map of service_principal_key values across all data_factory_credential_service_principals, keyed the same as var.data_factory_credential_service_principals"
+  value       = { for k, v in azurerm_data_factory_credential_service_principal.data_factory_credential_service_principals : k => v.service_principal_key }
 }
 output "data_factory_credential_service_principals_tenant_id" {
-  description = "List of tenant_id values across all data_factory_credential_service_principals"
-  value       = [for k, v in azurerm_data_factory_credential_service_principal.data_factory_credential_service_principals : v.tenant_id]
+  description = "Map of tenant_id values across all data_factory_credential_service_principals, keyed the same as var.data_factory_credential_service_principals"
+  value       = { for k, v in azurerm_data_factory_credential_service_principal.data_factory_credential_service_principals : k => v.tenant_id }
 }
 
