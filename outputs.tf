247c4b5f0d954174ccc8a09b1d23d3c2759a11fd
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:01:21 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "data_factory_credential_service_principals_id" {
+  description = "Map of id values across all data_factory_credential_service_principals, keyed the same as var.data_factory_credential_service_principals"
+  value       = { for k, v in azurerm_data_factory_credential_service_principal.data_factory_credential_service_principals : k => v.id }
+}
 output "data_factory_credential_service_principals_annotations" {
   description = "Map of annotations values across all data_factory_credential_service_principals, keyed the same as var.data_factory_credential_service_principals"
   value       = { for k, v in azurerm_data_factory_credential_service_principal.data_factory_credential_service_principals : k => v.annotations }
